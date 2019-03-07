0b02db3e8fdb29d1803f2635bb7d9a28a16d20e4
kurt miller <kmiller9393@gmail.com>
2019-03-07 12:52:35 -0700

    Add functionality to display message based on user's latitude and in the winter months or summer months

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>SeasonDisplay component.</div>;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = ({ latitude }) => {
+  const season = getSeason(latitude, new Date().getMonth());
+  const text =
+    season === 'winter' ? "Burr it's chilly!" : "Let's hit the beach!";
+
+  return <div>{text}</div>;
 };
 
 export default SeasonDisplay;
